8534171255d35e63cb6ce1bff486b6b2f758ce30
Maksim <dekstor235@gmail.com>
2023-03-06 19:35:10 +0300

    Created

--- linesymmetry.py
+++ linesymmetry.py
@@ -1,43 +1,54 @@
-# Lines of symetry with mtcnn
 import cv2
-from matplotlib import pyplot
-from matplotlib.patches import Rectangle
-from mtcnn.mtcnn import MTCNN
+import dlib
 
+desired_width = 469  # желаемая ширина
+desired_height = 640  # желаемая высота
+dim = (desired_width, desired_height)  # размер в итоге
 
-# создаем метод, чтобы он прорисовывал линии нобнаруженных точках
-def draw_image_with_boxes(filename, result_list):
-    # загрузить фотографию
-    data = pyplot.imread(filename)
-    # загрузить фото на график
-    pyplot.imshow(data)
-    # получение содержимого для прорисовки линий и прямоугольников
-    ax = pyplot.gca()
-    # просмотреть все лица
-    for result in result_list:
-        # получить координаты лиц
-        x, y, width, height = result['box']
-        # создание прямоуголников по координатам лица
-        rect = Rectangle((x, y), width, height, fill=False, color='red')
-        # добавление их на фото
-        ax.add_patch(rect)
-        # нарисовать линии симметрии
-        for key, value in result['keypoints'].items():
-            if key in ['left_eye', 'right_eye', 'nose']:
-               line = Rectangle((value[0], y), 0, height, fill=False, color='red')
-               ax.add_patch(line)
-        print("Расстояние локальных от центра:", abs(result['keypoints']['left_eye'][0] - result['keypoints']['nose'][0]),';',abs(result['keypoints']['right_eye'][0] - result['keypoints']['nose'][0]))
-    # показать итог
-    pyplot.show()
+x = []
+y = []
+i = 0
+points = [7, 20, 30, 36, 39, 42, 45] # 7 - край брови; 20 - край челюсти ;30 - кончик носа;
+                                     # 36, 39 - углы левого глаза; 42, 45 - углы правого глаза
 
+# Load the detector
+detector = dlib.get_frontal_face_detector()
+# Load the predictor
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+# read the image
+img = cv2.imread("ronaldu-13.jpg")
+# Convert image into grayscale
+gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+# Use detector to find landmarks
+faces = detector(gray)
+for face in faces:
+    x1 = face.left() # left point
+    y1 = face.top() # top point
+    x2 = face.right() # right point
+    y2 = face.bottom() # bottom point
+    # Look for the landmarks
+    landmarks = predictor(image=gray, box=face)
 
+    for n in points:
+        x.append(landmarks.part(n).x)
+        y.append(landmarks.part(n).y)
+        # Draw a circle
 
-filename = '3.jpg'
-# загрузить фотографию
-pixels = cv2.imread(filename)
-# Инициализировать mtcnn
-detector = MTCNN()
-# используя mtcnn получить координаты лиц и положение фраментов лица
-faces = detector.detect_faces(pixels)
-# нарисовать линии симметрии
-draw_image_with_boxes(filename, faces)
+        i+=1
+
+xres=x[1]+x[2]
+xres = xres//2
+cv2.line(img, ((x[3]+x[4])//2, y[0]), ((x[3]+x[4])//2, y[1]), (0, 255, 0), 2) #левая побочная линия симметрии
+cv2.line(img, ((x[5]+x[6])//2, y[0]), ((x[5]+x[6])//2, y[1]), (0, 255, 0), 2) #правая побочная линия симметрии
+cv2.line(img, (x[2], y[0]), (x[2], y[1]//2), (0, 255, 0), 2) #основная линия симметрии
+cv2.putText(img, 'r = ' + str(((x[5]+x[6])//2)-x[2]), (x[3], y[1]), cv2.FONT_HERSHEY_SIMPLEX,
+             0.75, (0, 255, 0), 2)
+cv2.putText(img, 'r = ' + str(x[2]-((x[3]+x[4])//2)), (x[5], y[1]), cv2.FONT_HERSHEY_SIMPLEX,
+             0.75, (0, 255, 0), 2)
+#img = cv2.resize(img, dim)
+# show the image
+cv2.imshow(winname="Face", mat=img)
+# Wait for a key press to exit
+cv2.waitKey(delay=0)
+# Close all windows
+cv2.destroyAllWindows()
